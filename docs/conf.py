e4db35bf33787c12861d5d2091c93a1b031ab8c0
taxguide <singhparash06@gmail.com>
2026-07-20 12:29:18 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'ancestry sign in problems'
+author = 'ancestry sign in problems'
+release = '1.0'
+
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "ancestry sign in problems"
+
+# Sitemap
+html_baseurl = "https://fidelity-fidelity-netbenefits-log-in.readthedocs-hosted.com/"
+sitemap_url_scheme = "{link}"
 
